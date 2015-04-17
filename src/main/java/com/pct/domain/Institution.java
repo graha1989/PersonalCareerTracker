9c3b3e63eeccba7d2aeeac63271d6e002a3aa96c
Aleksandar <Aleksandar@aleksandar-pc>
2015-04-17 16:06:15 +0200

    PCT_17042015: edit/create work experience fix

--- src/main/java/com/pct/domain/Institution.java
+++ src/main/java/com/pct/domain/Institution.java
@@ -5,7 +5,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -39,7 +38,7 @@ public class Institution extends AbstractEntity {
 	@Column(name = "city", length = 50)
 	private String city;
 
-	@OneToMany(fetch = FetchType.EAGER, mappedBy = "institution", cascade = CascadeType.ALL)
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "institution")
 	@JsonManagedReference(value = "institution")
 	private Set<WorkExperience> workExperiences = new HashSet<WorkExperience>();
 
